0571205fa01d1efd8ea7390882999e44d20ac684
davidepetrillo <davide96petrillo@gmail.com>
2022-05-19 17:19:22 +0200

    Aggiunto controller API e nuova HomePage

---
+++ LaMiaPizzeria/Controllers/API/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using LaMiaPizzeria.Data;
+using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaMiaPizzeria.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Pizze> pizze = new List<Pizze>();
+
+            using (PizzeContext db = new PizzeContext())
+            {
+                pizze = db.Pizze.ToList<Pizze>();
+            }
+
+            return Ok(pizze);
+        }
+
+    }
+}
